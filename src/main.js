64c4a9676a08a052cc5ce481eb3a5094a138c006
chandrasekharaRddy <supula263@gmail.com>
2024-06-13 20:29:44 +0530

    final program

--- src/main.js
+++ src/main.js
@@ -1,4 +1,48 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// main.js
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+import LoginForm from "./components/logiForm.vue";  
+import Dashboard from "./components/attendenceForm.vue"; 
+import Navigation from "./components/theNavigation.vue";  
+import members from "./components/teamMembers.vue";
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+import store from "./store/store"
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    component: LoginForm,
+    name: 'login',
+  },
+  {
+    path: '/attendence',
+    component: Dashboard,
+    name: 'attendence',
+    props: true,
+  },
+  {
+    path: '/navigation',
+    component: Navigation,
+    name: 'navigation',
+  },
+ 
+  {
+    path: "/members",
+    component: members,
+  }
+];
+const router = createRouter({
+  history: createWebHistory(),
+  linkActiveClass: 'active-link',
+  routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.component('VueDatePicker', VueDatePicker);
+app.mount('#app');
